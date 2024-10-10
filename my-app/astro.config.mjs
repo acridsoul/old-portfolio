6fbdb0895e0f08c3b2d28e972a8bd5a8c82901cf
acridsoul <skitts4code@proton.me>
2024-10-10 23:06:11 +0300

    follow rules

--- my-app/astro.config.mjs
+++ my-app/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://acridsoul.github.io',
-    base: '',
+    base: 'my-app',
 });
