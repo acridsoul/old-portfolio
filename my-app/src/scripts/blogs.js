bfa4df9c3c9eab5c5c06a37e80f8e54d4cca8eea
acridsoul <skitts4code@proton.me>
2024-10-25 03:35:29 +0300

    lazy fix to mobile view nav

--- my-app/src/scripts/blogs.js
+++ my-app/src/scripts/blogs.js
@@ -46,3 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
--- my-app/src/scripts/projects.js
+++ my-app/src/scripts/projects.js
@@ -10,3 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
   });
+
+  document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
