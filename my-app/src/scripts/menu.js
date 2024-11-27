d812861c90330dab0cae1159a628bd7426f8055c
acridsoul <skitts4code@proton.me>
2024-11-28 01:03:07 +0300

    add cursor effects

--- my-app/src/scripts/menu.js
+++ my-app/src/scripts/menu.js
@@ -1,3 +1,16 @@
+import { fairyDustCursor } from "cursor-effects";
+
+
+
+window.addEventListener("load", () => {
+  new fairyDustCursor(
+    {
+      colors: ["#800080", "#00ff00", "#fff"],
+    }
+  ); // No element parameter, applies to the whole body
+});
+
+
 // js code for the clock
 setInterval(myClock, 0);
 function myClock() {
