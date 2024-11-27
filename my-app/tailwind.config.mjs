027eed05ff1aaa7eee6b0e7f82377a2774623abe
acridsoul <skitts4code@proton.me>
2024-11-27 15:50:08 +0300

    remove nav and add tailwind

--- my-app/tailwind.config.mjs
+++ my-app/tailwind.config.mjs
@@ -5,7 +5,11 @@ export default {
 	  },
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'custom-green': '#36e414',
+			},
+		},
 	},
 	plugins: [],
 }
