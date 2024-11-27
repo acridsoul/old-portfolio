8a60ae2ae96603efc7e60991a52d1990a473a6ca
acridsoul <skitts4code@proton.me>
2024-11-28 02:32:39 +0300

    style webring

--- my-app/tailwind.config.mjs
+++ my-app/tailwind.config.mjs
@@ -12,6 +12,9 @@ export default {
 			fontFamily: {
 				iceland: ['Iceland', 'sans-serif']
 			},
+			backgroundImage: {
+				'gradient-rainbow': 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)',
+			},
 		},
 	},
 	plugins: [],
