42250023f34cf4e40790c660e287701555360da3
acridsoul <skitts4code@proton.me>
2024-11-29 13:18:39 +0300

    add badges

--- my-app/tailwind.config.mjs
+++ my-app/tailwind.config.mjs
@@ -15,7 +15,13 @@ export default {
 			backgroundImage: {
 				'gradient-rainbow': 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)',
 			},
+			textShadow: {
+				'blue': '2px 2px 4px rgba(0, 0, 255, 0.5)',
+				'glow': '0 0 10px rgba(255, 255, 255, 0.8), 0 0 20px rgba(255, 255, 255, 0.6), 0 0 30px rgba(255, 255, 255, 0.4)',
+			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-textshadow')
+	],
 }
